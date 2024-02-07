9d994d101b800136e74a03ea3a333424bbd32e9d
NaziaHladun <fabulous7351@gmail.com>
2024-02-07 18:46:39 +0200

    created 'no project page' and added create page opening

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import React, { useState } from "react";
+
 import Sidebar from "./components/Sidebar";
 import NewProject from "./components/NewProject";
+import NoProject from "./components/NoProject";
 
 function App() {
+  const [newProject, setNewProject] = useState(false);
+
+  function createNewProject() {
+    setNewProject(true);
+  }
+
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
-        <Sidebar />
-        <NewProject />
+        <Sidebar onCreate={setNewProject} />
+        {newProject ? (
+          <NewProject onCancel={setNewProject} />
+        ) : (
+          <NoProject onCreate={setNewProject} />
+        )}
       </main>
     </>
   );
---
+++ src/components/NoProject.jsx
@@ -0,0 +1,22 @@
+import CreateButton from "./CreateButton";
+
+export default function NoProject({ onCreate }) {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img
+        className="w-16 h-16 object-contain mx-auto"
+        src="./src/assets/no-projects.png"
+        alt="project image"
+      />
+      <h2 className="text-xl font-bold text-stone-500 my-4">
+        No Project Selected
+      </h2>
+      <p className="text-stone-400 mb-4">Select a project or create new one.</p>
+      <p>
+        <CreateButton onClick={() => onCreate(true)}>
+          Create new project
+        </CreateButton>
+      </p>
+    </div>
+  );
+}
